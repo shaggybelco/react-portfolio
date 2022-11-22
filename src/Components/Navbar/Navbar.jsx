7b31c2b50cdad89ad660404b69aa1a54eac887a3
shaggybelco <shaggybelco1@gmail.com>
2022-11-22 13:53:24 -0800

    skills styles

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ export class Navbar extends Component {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
                         </div>
-                        <div className="flex-1 px-2 mx-2">Shaggy Belco Sambo <sub className='ml-5'>FullStack developer</sub></div>
+                        <div className="flex-1 px-2 mx-2 font-bold">Shaggy Belco Sambo <sub className='ml-5 font-normal'>FullStack developer</sub></div>
                         <div className="flex-none hidden lg:block">
                             <ul className="menu menu-horizontal">
                             {/* desktop */}
