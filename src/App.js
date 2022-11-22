6bb61b8e3412ae94718e247aeeacc3035a8375ff
shaggybelco <shaggybelco1@gmail.com>
2022-11-22 11:46:34 -0800

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div><Home></Home></div>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;
